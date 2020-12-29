4a6d8c087ae10622e11742f1e0aed85e74d2f418
serendatapy <arvaldivia@gmail.com>
2020-12-29 20:26:05 +0100

    Refactor: fixed dependencies and test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import 'jest-canvas-mock'
+import { createMemoryHistory } from 'history'
+import { Router } from 'react-router-dom'
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Huntdora/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders welcome', () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+  // const textElement = screen.getByt(/Huntdora/i);
+  // expect(textElement).toBeInTheDocument();
 });
